4de87737553ca23885b61b0e515ef0df7ccdcb65
Escape <Escape>
2025-09-14 18:32:59 +0530

    remove lap

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
 // return b
 // }
 // shout("fire")
+
+// Device type	Typical breakpoint	Media query example
+// Extra small (phones)	≤ 480 px	@media (max-width: 480px) { … }
+// Small (phones)	≤ 576 px	@media (max-width: 576px) { … }
+// Medium (tablets)	≥ 768 px	@media (min-width: 768px) { … }
+// Large (small laptops)	≥ 992 px	@media (min-width: 992px) { … }
+// Extra large (desktops)	≥ 1200 px	@media (min-width: 1200px) { … }
+// XXL (wide screens)	≥ 1400 px	@media (min-width: 1400px) { … }
